5468a7b7e0ab54565161174817715ab1315b7938
Ameesha <ameeshaagrawal12@gmail.com>
2022-02-22 01:08:43 +0530

    Initial setup

---
+++ hardhat.config.js
@@ -0,0 +1,306 @@
+require("dotenv").config();
+
+require("solidity-coverage");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("hardhat-gas-reporter");
+require("hardhat-spdx-license-identifier");
+require("hardhat-watcher");
+require("hardhat-contract-sizer");
+
+const accounts =
+  process.env.DEPLOYER_PRIVATE_KEY !== undefined
+    ? [process.env.DEPLOYER_PRIVATE_KEY]
+    : [];
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  abiExporter: {
+    path: "./abi",
+    clear: false,
+    flat: true,
+  },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey:
+      process.env.ETHERSCAN_API_KEY !== undefined
+        ? [process.env.ETHERSCAN_API_KEY]
+        : [],
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    excludeContracts: ["contracts/mock/"],
+  },
+  mocha: {
+    timeout: 20000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
+      gasPrice: 120 * 1000000000,
+      chainId: 1,
+    },
+    hardhat: {
+      saveDeployments: true,
+      tags: ["test", "local"],
+      allowUnlimitedContractSize: true,
+      accounts: {
+        accountsBalance: "10000000000000000000000000",
+      },
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
+      accounts,
+      chainId: 3,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
+      accounts,
+      chainId: 4,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
+      accounts,
+      chainId: 5,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
+      accounts,
+      chainId: 42,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 20000000000,
+      gasMultiplier: 2,
+    },
+    moonbase: {
+      url: "https://rpc.testnet.moonbeam.network",
+      accounts,
+      chainId: 1287,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gas: 5198000,
+      gasMultiplier: 2,
+    },
+    fantom: {
+      url: "https://rpcapi.fantom.network",
+      accounts,
+      chainId: 250,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 22000000000,
+    },
+    "fantom-testnet": {
+      url: "https://rpc.testnet.fantom.network",
+      accounts,
+      chainId: 4002,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    matic: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts,
+      chainId: 137,
+      live: true,
+      saveDeployments: true,
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts,
+      chainId: 80001,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    xdai: {
+      url: "https://rpc.xdaichain.com",
+      accounts,
+      chainId: 100,
+      live: true,
+      saveDeployments: true,
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org",
+      accounts,
+      chainId: 56,
+      live: true,
+      saveDeployments: true,
+    },
+    "bsc-testnet": {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      accounts,
+      chainId: 97,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    heco: {
+      url: "https://http-mainnet.hecochain.com",
+      accounts,
+      chainId: 128,
+      live: true,
+      saveDeployments: true,
+    },
+    "heco-testnet": {
+      url: "https://http-testnet.hecochain.com",
+      accounts,
+      chainId: 256,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      accounts,
+      chainId: 43114,
+      live: true,
+      saveDeployments: true,
+      gasPrice: 470000000000,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts,
+      chainId: 43113,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    harmony: {
+      url: "https://api.s0.t.hmny.io",
+      accounts,
+      chainId: 1666600000,
+      live: true,
+      saveDeployments: true,
+    },
+    "harmony-testnet": {
+      url: "https://api.s0.b.hmny.io",
+      accounts,
+      chainId: 1666700000,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    okex: {
+      url: "https://exchainrpc.okex.org",
+      accounts,
+      chainId: 66,
+      live: true,
+      saveDeployments: true,
+    },
+    "okex-testnet": {
+      url: "https://exchaintestrpc.okex.org",
+      accounts,
+      chainId: 65,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    arbitrum: {
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts,
+      chainId: 42161,
+      live: true,
+      saveDeployments: true,
+      blockGasLimit: 700000,
+    },
+    "arbitrum-testnet": {
+      url: "https://kovan3.arbitrum.io/rpc",
+      accounts,
+      chainId: 79377087078960,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasMultiplier: 2,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts,
+      chainId: 42220,
+      live: true,
+      saveDeployments: true,
+    },
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT,
+    username: process.env.TENDERLY_USERNAME,
+  },
+  watcher: {
+    compile: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+    },
+  },
+};
